b506b2f32c2354eb8974903e0f623a87686af5bd
elhambanan <elham.banagozar@gmail.com>
2022-02-06 16:00:37 +0330

    Raise an event

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,13 +1,37 @@
 // import "./product.css"
 import style from "./product.module.css"
+import { BiTrash } from "react-icons/bi";
 
 const Product = (props ) => {
     // console.log(props)
     return(
         <div className={style.product} onClick={props.click}>
-            <p>product name: {props.name} </p>
-            <p>product price: {props.price} </p>
-            {props.children}
+            <p>product name: {props.products.title} </p>
+            <p>product price: {props.products.price} </p>
+            <span className={style.value} >
+                {props.products.quantity}
+            </span>
+            <input type="text" className={style.input}
+                    onChange={props.onChange}
+                    value={props.products.title}/>
+            <button 
+                    className={`
+                    ${style.btn} 
+                    ${props.products.quantity === 1 && style.remove}`}
+                    onClick={props.onDecreament}>
+                    {props.products.quantity>1 ? "-" : <BiTrash/>}
+            </button>        
+            <button 
+                    className={`${style.btn} ${style.inc}`}
+                    onClick={props.onIncreament}>
+                    +
+            </button>
+           
+            <button 
+                    className={style.btn} 
+                    onClick={props.onDelete}>
+                    delete
+            </button>
         </div>
     )
 }
--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,30 +1,84 @@
 import React, { Component } from 'react';
 import Product from '../Product/Product';
 
+
 class ProductList extends Component {
     state = { 
          products : [
-        {title: "react.js", price: "99 $"},
-        {title: "node.js", price: "78 $"},
-        {title: "veu.js", price: "45 $"},
-    ], 
-};
+        {title: "react.js", price: "99 $", id: 1, quantity: 1},
+        {title: "node.js", price: "78 $", id: 2, quantity: 3},
+        {title: "veu.js", price: "45 $", id: 3, quantity: 5},
+        ], 
+    };
+    // state => Handler
+    removeHandler = (id)=>{
+        console.log("remove", id);
+        const filteredProducts = this.state.products.filter((p) => p.id !== id);
+        this.setState({products : filteredProducts})
+    }
+    incHandler = (id) => {
+        const products = this.state.products //clone => not mutate state
+
+        const selectedItem = products.find(p => p.id === id);
+        selectedItem.quantity++;
+        this.setState({products: products}) 
+    }
+    decHandler = (id) => {
+        const products = this.state.products //clone => not mutate state
+
+        const selectedItem = products.find(p => p.id === id);
+        if(selectedItem.quantity === 1) {
+            const filteredProducts = products.filter((p) => p.id !== id);
+            this.setState({products: filteredProducts})
+            console.log(id, filteredProducts)
+        }else { 
+            selectedItem.quantity--;
+            this.setState({products: products}) }
+    }
+    changeHandler = (e, id) => {
+        const products = this.state.products;
+        
+        const selectedItem = products.find( p => p.id === id);
+        selectedItem.title = e.target.value;
+
+        this.setState({products : products})
+    }
+    renderProduct = ()=> {
+        if(this.state.products.length === 0)
+            return <div>there is no Product</div>
+        return (
+            <div>
+                {this.state.products.map((p, index) => {
+                return (
+                <Product
+                    // name = {p.title}
+                    // price = {p.price}
+                    // quantity = {p.quantity}
+                    // key = {p.id}
+                    products = {p}
+                    onDelete = {() => this.removeHandler(p.id)}
+                    onIncreament = {() => this.incHandler(p.id)}
+                    onDecreament = {() => this.decHandler(p.id)}
+                    onChange = {(e) => this.changeHandler(e, p.id)}
+                    // click = {this.clickHandler.bind(this, "newTilte")}
+                    // click = {() => this.clickHandler("newTilte")}
+                />
+                )
+                })}
+            </div>
+        );    
+    }
     render() { 
         return (
             <div>
-               {this.state.products.map((p, index) => {
-               return (
-                <Product
-                    name = {p.title}
-                    price = {p.price}
-                    key = {index}
-                    // click = {this.clickHandler.bind(this, "newTilte")}
-                    click = {() => this.clickHandler("newTilte")}
-                />
-    )
-})}
+                {!this.state.products.length 
+                ?(<div>go to Shopping</div> )
+                :(null )
+                }
+                {this.renderProduct()}
             </div>
-        );
+        )
+            
     }
 }
  
