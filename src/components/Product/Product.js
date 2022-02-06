65a23c019ba256a277f35d6f0618c5a0721a27f5
elhambanan <elham.banagozar@gmail.com>
2022-02-06 16:00:37 +0330

    Raise an event

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,12 +2,14 @@
 import style from "./product.module.css"
 
 const Product = (props ) => {
-    // console.log(props)
+    console.log(props)
     return(
         <div className={style.product} onClick={props.click}>
-            <p>product name: {props.name} </p>
-            <p>product price: {props.price} </p>
-            {props.children}
+            <p>product name: {props.products.name} </p>
+            <p>product price: {props.products.price} </p>
+            <button className={style.product}>increament</button>
+            <span>{props.products.quantity}</span>
+            <button onClick={props.products.onDelete}>delete</button>
         </div>
     )
 }
--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -4,25 +4,34 @@ import Product from '../Product/Product';
 class ProductList extends Component {
     state = { 
          products : [
-        {title: "react.js", price: "99 $"},
-        {title: "node.js", price: "78 $"},
-        {title: "veu.js", price: "45 $"},
-    ], 
-};
+        {title: "react.js", price: "99 $", id: 1, quantity: 1},
+        {title: "node.js", price: "78 $", id: 2, quantity: 3},
+        {title: "veu.js", price: "45 $", id: 3, quantity: 5},
+        ], 
+    };
+    // state => Handler
+    removeHandler = (id)=>{
+        console.log("remove", id);
+        const filteredProducts = this.state.products.filter((p) => p.id !== id);
+        this.setState({products : filteredProducts})
+    }
     render() { 
         return (
             <div>
                {this.state.products.map((p, index) => {
                return (
                 <Product
-                    name = {p.title}
-                    price = {p.price}
-                    key = {index}
+                    // name = {p.title}
+                    // price = {p.price}
+                    // quantity = {p.quantity}
+                    // key = {p.id}
+                    products = {p}
+                    onDelete = {() => this.removeHandler(p.id)}
                     // click = {this.clickHandler.bind(this, "newTilte")}
-                    click = {() => this.clickHandler("newTilte")}
+                    // click = {() => this.clickHandler("newTilte")}
                 />
-    )
-})}
+                )
+              })}
             </div>
         );
     }
